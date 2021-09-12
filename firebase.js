cfffca5adf3d41668d8083020e256cab2f408978
sweta kumari <skumari943137@outlook.com>
2021-09-13 01:51:00 +0530

    First Commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA7JygDyDy268237DGvTXEtgygibMdoVBY",
+    authDomain: "spiffy-15281.firebaseapp.com",
+    projectId: "spiffy-15281",
+    storageBucket: "spiffy-15281.appspot.com",
+    messagingSenderId: "1000312744178",
+    appId: "1:1000312744178:web:a13ab07b41fbdb1e08886b",
+    measurementId: "G-17Q6XSY8HD"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
